a5c65a5a40ff81df210f8a4714e32605ea405c62
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2022-05-11 01:57:22 +0600

    updating user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,4 +11,26 @@ export class UsersService {
         const user = this.repo.create({ email, password });
         return this.repo.save(user);
     }
+
+    fundOne(id: number) {
+        return this.repo.findOne(id);
+    }
+
+    find(email: string) {
+        return this.repo.find({ email: email });
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.fundOne(id);
+        if (!user) {
+            throw new Error('user not found');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    // remove(id: number) {
+    //     const user = this.repo.remove(id);
+    //     return this.repo.delete(user);
+    // }
 }
