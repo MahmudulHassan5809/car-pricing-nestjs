576e028b647d74108ba58266aff9b1aa3fcaf1c8
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2022-05-15 00:48:23 +0600

    typeorm query builder

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,11 +3,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../users/user.entity';
 import { CreateReportDto } from './dtos/create-report-dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { Report } from './report.entity';
 
 @Injectable()
 export class ReportsService {
     constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+    createEstimate({ make, lat, lng, mileage }: GetEstimateDto) {
+        return (
+            this.repo
+                .createQueryBuilder()
+                // .select('*')
+                .select('AVG(price)', 'price')
+                .where('make = :make', { make })
+                .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+                .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+                .andWhere('approved IS TRUE')
+                .orderBy('ABS(mileage - :mileage)', 'DESC')
+                .setParameters({ mileage })
+                .getRawMany()
+        );
+    }
 
     create(reportDto: CreateReportDto, user: User) {
         const report = this.repo.create(reportDto);
